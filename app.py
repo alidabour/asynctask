eb934c7515c931393888f636c955c8de9e62d3bd
alidabour <alidabour0@gmail.com>
2017-11-26 09:53:11 +0200

    workers = 4

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ app = Sanic()
 async def test(request):
     return json({"hello": "Sanic"})
 
-asyncio.set_event_loop(uvloop.new_event_loop())
-server = app.create_server(host="0.0.0.0",port=8000)
-loop = asyncio.get_event_loop()
-task = asyncio.ensure_future(server)
-signal(SIGINT,lambda s,f: loop.stop())
-try:
-	loop.run_forever()
-except:
-	loop.stop()
-# if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=8000, debug = False)
+# asyncio.set_event_loop(uvloop.new_event_loop())
+# server = app.create_server(host="0.0.0.0",port=8000)
+# loop = asyncio.get_event_loop()
+# task = asyncio.ensure_future(server)
+# signal(SIGINT,lambda s,f: loop.stop())
+# try:
+# 	loop.run_forever()
+# except:
+# 	loop.stop()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug = False,workers =4)

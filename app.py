a7cc53cacc264698014a45b77e6bc558218d53c4
alidabour <alidabour0@gmail.com>
2017-11-26 09:36:58 +0200

    create server with uvloop

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from sanic import Sanic
 from sanic.response import json
+from signal import signal,SIGINT
+from multiprocessing import Event
+import uvloop
+import asyncio
 
+# asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 app = Sanic()
 
 
@@ -8,6 +13,14 @@ app = Sanic()
 async def test(request):
     return json({"hello": "Sanic"})
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug = False)
+asyncio.set_event_loop(uvloop.new_event_loop())
+server = app.create_server(host="0.0.0.0",port=8001)
+loop = asyncio.get_event_loop()
+task = asyncio.ensure_future(server)
+signal(SIGINT,lambda s,f: loop.stop())
+try:
+	loop.run_forever()
+except:
+	loop.stop()
+# if __name__ == "__main__":
+    # app.run(host="0.0.0.0", port=8000, debug = False)

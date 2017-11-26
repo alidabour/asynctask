f12f682a74e6083cb7f8d58db5fc66866e9f5816
alidabour <alidabour0@gmail.com>
2017-11-26 09:48:40 +0200

    port 8000

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ async def test(request):
     return json({"hello": "Sanic"})
 
 asyncio.set_event_loop(uvloop.new_event_loop())
-server = app.create_server(host="0.0.0.0",port=8000,debug = False)
+server = app.create_server(host="0.0.0.0",port=8000)
 loop = asyncio.get_event_loop()
 task = asyncio.ensure_future(server)
 signal(SIGINT,lambda s,f: loop.stop())

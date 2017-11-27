024ec76cca52a2e746f6644207bf767b69949808
alidabour <alidabour0@gmail.com>
2017-11-27 22:19:54 +0200

    fix spaces

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Sanic()
 
 @app.route("/adserver")
 async def test(request):
-    conn = await asyncpg.connect('postgres://aglziifkvghija:e88c7cf121436073c5f21b1d658e1b0d2250d697a281b23ad129564d8fa39226@ec2-174-129-193-169.compute-1.amazonaws.com:5432/dbda36k41dj2f2',password='e88c7cf121436073c5f21b1d658e1b0d2250d697a281b23ad129564d8fa39226')
+	conn = await asyncpg.connect('postgres://aglziifkvghija:e88c7cf121436073c5f21b1d658e1b0d2250d697a281b23ad129564d8fa39226@ec2-174-129-193-169.compute-1.amazonaws.com:5432/dbda36k41dj2f2',password='e88c7cf121436073c5f21b1d658e1b0d2250d697a281b23ad129564d8fa39226')
 	await conn.execute('''
 		INSERT INTO logquery(query) VALUES($1)
 		''', str(request.args))

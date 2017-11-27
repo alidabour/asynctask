020efb0d88268da4843de7afe1c54151c7aaceee
alidabour <alidabour0@gmail.com>
2017-11-27 23:30:12 +0200

    Test without database

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ app = Sanic()
 
 @app.route("/adserver")
 async def test(request):
-	conn = await asyncpg.connect('postgres://aglziifkvghija:e88c7cf121436073c5f21b1d658e1b0d2250d697a281b23ad129564d8fa39226@ec2-174-129-193-169.compute-1.amazonaws.com:5432/dbda36k41dj2f2',password='e88c7cf121436073c5f21b1d658e1b0d2250d697a281b23ad129564d8fa39226')
-	await conn.execute('''
-		INSERT INTO logquery(query) VALUES($1)
-		''', str(request.args))
-	await conn.close()
+	# conn = await asyncpg.connect('postgres://aglziifkvghija:e88c7cf121436073c5f21b1d658e1b0d2250d697a281b23ad129564d8fa39226@ec2-174-129-193-169.compute-1.amazonaws.com:5432/dbda36k41dj2f2',password='e88c7cf121436073c5f21b1d658e1b0d2250d697a281b23ad129564d8fa39226')
+	# await conn.execute('''
+	# 	INSERT INTO logquery(query) VALUES($1)
+	# 	''', str(request.args))
+	# await conn.close()
 	return json({"hello": request.args})
 
 if __name__ == "__main__":

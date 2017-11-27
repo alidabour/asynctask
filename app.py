83c3feff02cc8f3bbbc11316a18622619efa9f16
alidabour <alidabour0@gmail.com>
2017-11-28 00:20:55 +0200

    clean up

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from sanic import Sanic
 from sanic.response import text
-from signal import signal,SIGINT
-from multiprocessing import Event
-import uvloop
 import asyncio
 import asyncpg
 
@@ -11,11 +8,11 @@ app = Sanic()
 
 @app.route("/adserver")
 async def test(request):
-	# conn = await asyncpg.connect('postgres://aglziifkvghija:e88c7cf121436073c5f21b1d658e1b0d2250d697a281b23ad129564d8fa39226@ec2-174-129-193-169.compute-1.amazonaws.com:5432/dbda36k41dj2f2',password='e88c7cf121436073c5f21b1d658e1b0d2250d697a281b23ad129564d8fa39226')
-	# await conn.execute('''
-	# 	INSERT INTO logquery(query) VALUES($1)
-	# 	''', str(request.args))
-	# await conn.close()
+	conn = await asyncpg.connect('postgres://aglziifkvghija:e88c7cf121436073c5f21b1d658e1b0d2250d697a281b23ad129564d8fa39226@ec2-174-129-193-169.compute-1.amazonaws.com:5432/dbda36k41dj2f2',password='e88c7cf121436073c5f21b1d658e1b0d2250d697a281b23ad129564d8fa39226')
+	await conn.execute('''
+		INSERT INTO logquery(query) VALUES($1)
+		''', str(request.args))
+	await conn.close()
 	return text("OK",status=200)
 
 if __name__ == "__main__":

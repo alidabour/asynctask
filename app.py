51f0d57c26c48f5b6af24bea35f6efe95c3c7287
alidabour <alidabour0@gmail.com>
2017-11-29 07:52:00 +0200

    fix

--- app.py
+++ app.py
@@ -21,8 +21,7 @@ async def connectDB():
 	# conn = await asyncpg.connect('postgres://aglziifkvghija:e88c7cf121436073c5f21b1d658e1b0d2250d697a281b23ad129564d8fa39226@ec2-174-129-193-169.compute-1.amazonaws.com:5432/dbda36k41dj2f2',password='e88c7cf121436073c5f21b1d658e1b0d2250d697a281b23ad129564d8fa39226')
 	conn = await asyncpg.connect(database=url.path[1:],
 		user=url.username,
-		password=url.password,
-		host=url.hostname)
+		password=url.password)
 	return conn
 
 async def insert_query(conn,query):
